654ccd668ec7af0e863432d1449f56ccd23194b3
Sylar <redpeachj@gmail.com>
2019-01-15 04:31:42 +0000

    创建会话

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,7 +20,7 @@ class UsersController extends Controller
     public function store(Request $request)
     {
         $this->validate($request,[
-            'name'=>'required|min:6|max:50',
+            'name'=>'required|min:2|max:50',
             'email'=>'required|email|unique:users|max:255',
             'password'=>'required|confirmed|min:6'
         ]);
