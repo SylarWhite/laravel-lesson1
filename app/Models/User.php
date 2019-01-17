79e758074d171024f5e2089d4d54b9f804f23934
Sylar <redpeachj@gmail.com>
2019-01-17 02:46:04 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable
 {
@@ -30,16 +31,75 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    /**
+     * 粉丝
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function followers()
+    {
+        return $this->belongsToMany(Follower::class,'followers','user_id','follower_id');
+    }
 
+    /**
+     * 关注对象
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function followings(){
+        return $this->belongsToMany(Follower::class,'followers','follower_id','user_id');
+    }
+
+    /**
+     * 说说流
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function feed()
     {
         return $this->statuses()->orderBy('created_at','desc');
     }
 
+    /**
+     * 头像
+     * @param string $size
+     * @return string
+     */
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
+
+    /**
+     * 关注
+     * @param $user_ids
+     */
+    public function follow($user_ids)
+    {
+        if(!is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids,false);
+    }
+
+    /**
+     * 取关
+     * @param $user_ids
+     */
+    public function unFollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    /**
+     * 是否已关注
+     * @param $user_id
+     * @return mixed
+     */
+    public function isFollowing($user_id)
+    {
+        return $this->followings()->contains($user_id);
     }
 
     public static function boot()
@@ -51,6 +111,10 @@ class User extends Authenticatable
         });
     }
 
+    /**
+     * 说说
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function statuses()
     {
         return $this->hasMany(Status::class);
